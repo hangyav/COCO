1d0d51ab9253fdd848f54f977aa1075568220bbc
Krisztian Koos <Koos.Krisztian@stud.u-szeged.hu>
2010-11-08 21:58:39 +0100

    fill holes

--- src/COCO_.java
+++ src/COCO_.java
@@ -20,22 +20,31 @@ public class COCO_ implements PlugInFilter{
 	*/
 	public int setup(String arg, ImagePlus im){
 		image = im;
+		new ImageConverter(image).convertToGray8();
 		return DOES_ALL; //Milyen kepeket kezel a plugin (ALL)
 	}
 
 	/**
-	* A metodus hajtja vegre a megfelelo trszformaciot az kapott
+	* A metodus hajtja vegre a megfelelo trszformaciot a kapott
 	* {@code ImageProcessor} segitsegevel.
 	*
 	* @param ip Az objektum kezeli a kepet.
 	*/
-	public void run(ImageProcessor ip2){
-		new ImageConverter(image).convertToGray8();
-		ImageProcessor ip = image.getProcessor();
+	public void run(ImageProcessor ip){
+		//new ImageConverter(image).convertToGray8();
+		//ImageProcessor ip = image.getProcessor();
 		ip.autoThreshold();
+		//elkereses ide
 		BinaryProcessor bp = new BinaryProcessor(new ByteProcessor(ip.getBufferedImage()));
-		bp.dilate();
 		bp.erode();
+		bp.erode();
+		bp.dilate();
+		bp.dilate();
+		Binary b = new Binary();
+		b.setup("fill", image);
+		b.run(ip);
+		//FloodFiller ff = new FloodFiller(ip);
+		//ff.particleAnalyzerFill();
 		image.updateAndDraw();
 	}
 }
